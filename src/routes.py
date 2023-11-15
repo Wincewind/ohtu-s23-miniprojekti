f342f2cf49ea2d38674d3ba8a32c5e3a4c7a75a0
Martin Häggblom <martin.haggblom@gmail.com>
2023-11-15 15:41:04 +0200

    add local test method

--- src/routes.py
+++ src/routes.py
@@ -4,3 +4,16 @@ from flask import request, render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+# creating user
+#@app.route("/add_reference", methods=["POST"])
+#def add_reference():
+    #if request.method == "POST":
+        #author = request.form["author"]
+        #title = request.form["title"]
+        #year = request.form ["year"]
+        #publisher = request.form ["publisher"]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
