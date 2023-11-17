de93a170fffb63307c4a7ba12dfd2e7c5579fd9f
yogajones <95644662+yogajones@users.noreply.github.com>
2023-11-17 18:21:36 +0200

    Fix incomplete command and update frontend build

--- src/dataprocessing.py
+++ src/dataprocessing.py
@@ -4,7 +4,7 @@ from db import db
 def add_book(author, title, year, publisher, publisher_address):
     #inserting the new book into Books table
     db.session.execute(
-        text("""INSERT INTO Books (author, title, publication_year, publisher)
+        text("""INSERT INTO Books (author, title, publication_year, publisher, publisher_address)
                 VALUES (:author, :title, :year, :publisher, :publisher_address)"""),
         {"author": author, "title": title, "year": year, "publisher": publisher, "publisher_address": publisher_address}
     )
