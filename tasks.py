e9a47e919f7d0e1dff77ae8c62fcb14672c7ad17
yogajones <95644662+yogajones@users.noreply.github.com>
2023-11-26 15:02:48 +0200

    Add Invoke tasks

---
+++ tasks.py
@@ -0,0 +1,39 @@
+import os
+from invoke import task
+
+"""All tasks require a running PostegreSQL server."""
+
+
+@task
+def build_frontend(ctx):
+    os.chdir('frontend')
+    ctx.run('npm install', pty=True)
+    os.chdir('..')
+
+
+@task
+def build_backend(ctx):
+    ctx.run("poetry install && \
+            poetry shell",
+            pty=True)
+
+
+@task
+def start(ctx):
+    ctx.run("flask --app src/app.py run", pty=True)
+
+
+@task
+def init_db(ctx):
+    ctx.run("psql < schema.sql", pty=True)
+
+
+@task(build_frontend, build_backend)
+def build_full(ctx):
+    pass
+
+
+@task(init_db, build_frontend, build_backend)
+def build_full_init_db(ctx):
+    """CAUTION: Will delete any and all existing data!"""
+    pass
