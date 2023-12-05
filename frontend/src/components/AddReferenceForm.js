4746ce67c7f911b78fcad920794dd2493bcc9b48
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-12-05 19:36:31 +0200

    Add input field and logic for fetching DOI

--- frontend/src/components/AddReferenceForm.js
+++ frontend/src/components/AddReferenceForm.js
@@ -1,9 +1,11 @@
+import React, { useState } from 'react'
 import { addReference } from '../api/referenceService'
 
-const Form = ({ onReferenceAdded: fetchReferences }) => {
+const AddReferenceForm = ({ onReferenceAdded: fetchReferences }) => {
+    const [formData, setFormData] = useState({})
+
     const handleSubmit = async (event) => {
         event.preventDefault()
-        const formData = new FormData(event.target)
 
         try {
             await addReference(formData)
@@ -16,37 +18,132 @@ const Form = ({ onReferenceAdded: fetchReferences }) => {
         }
     }
 
+    const handleGetDOI = async (event) => {
+        event.preventDefault()
+        const doi = new FormData(event.target).get('DOI')
+        const url = `https://api.crossref.org/works/${doi}`
+
+        try {
+            const response = await fetch(url)
+            const data = await response.json()
+
+            if (response.ok && data.message) {
+                const metadata = data.message
+                setFormData({
+                    authors:
+                        metadata?.author
+                            ?.map(
+                                (author) => `${author.given} ${author.family}`
+                            )
+                            .join(', ') || '',
+                    title: metadata?.title?.[0] || '',
+                    year:
+                        metadata?.published[
+                            'date-parts'
+                        ]?.[0]?.[0]?.toString() || '',
+                    publisher: metadata?.publisher || '',
+                    publisher_address: metadata['publisher-location'] || '',
+                })
+            } else {
+                alert('DOI not found or metadata is incomplete.')
+            }
+        } catch (error) {
+            console.error('Fetching DOI failed:', error)
+            alert(`Couldn't find DOI!`)
+        }
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Authors: <input name="authors" type="text" />
-            </label>
-            <br />
-            <br />
-            <label>
-                Title: <input name="title" type="text" />
-            </label>
-            <br />
-            <br />
-            <label>
-                Year: <input name="year" type="text" />
-            </label>
-            <br />
-            <br />
-            <label>
-                Publisher: <input name="publisher" type="text" />
-            </label>
-            <br />
-            <br />
-            <label>
-                Publisher's address:
-                <input name="publisher_address" type="text" />
-            </label>
-            <br />
-            <br />
-            <button type="submit">Submit</button>
-        </form>
+        <>
+            <form onSubmit={handleGetDOI}>
+                <label>
+                    Enter DOI: <input name="DOI" type="text" />
+                </label>
+                <br />
+                <br />
+                <button type="submit">Get DOI</button>
+                <br />
+                <br />
+            </form>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Authors:
+                    <input
+                        name="authors"
+                        type="text"
+                        value={formData.authors || ''}
+                        onChange={(e) =>
+                            setFormData({
+                                ...formData,
+                                authors: e.target.value,
+                            })
+                        }
+                    />
+                </label>
+                <br />
+                <br />
+                <label>
+                    Title:
+                    <input
+                        name="title"
+                        type="text"
+                        value={formData.title || ''}
+                        onChange={(e) =>
+                            setFormData({ ...formData, title: e.target.value })
+                        }
+                    />
+                </label>
+                <br />
+                <br />
+                <label>
+                    Year:
+                    <input
+                        name="year"
+                        type="text"
+                        value={formData.year || ''}
+                        onChange={(e) =>
+                            setFormData({ ...formData, year: e.target.value })
+                        }
+                    />
+                </label>
+                <br />
+                <br />
+                <label>
+                    Publisher:
+                    <input
+                        name="publisher"
+                        type="text"
+                        value={formData.publisher || ''}
+                        onChange={(e) =>
+                            setFormData({
+                                ...formData,
+                                publisher: e.target.value,
+                            })
+                        }
+                    />
+                </label>
+                <br />
+                <br />
+                <label>
+                    Publisher's address:
+                    <input
+                        name="publisher_address"
+                        type="text"
+                        value={formData.publisher_address || ''}
+                        onChange={(e) =>
+                            setFormData({
+                                ...formData,
+                                publisher_address: e.target.value,
+                            })
+                        }
+                    />
+                </label>
+                <br />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+        </>
     )
 }
 
-export default Form
+export default AddReferenceForm
