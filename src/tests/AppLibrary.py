9842dc8562a0e6c3176b42cc83991edf0e06ce46
Wincewind <86107954+Wincewind@users.noreply.github.com>
2023-12-07 17:47:15 +0200

    WIP function for reading the generated bibtex file added

--- src/tests/AppLibrary.py
+++ src/tests/AppLibrary.py
@@ -1,5 +1,10 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+FORM_ELEMENTS = ['authors', 'title', 'year', 'publisher', 'publisher_address']
+TEST_INPUT_1 = ['Mariot Tsitoara', 'Beginning Git and GitHub', '2019', 'APress', 'One New York Plaza, Suite 4600 New York, NY']
+
 
 class AppLibrary:
     def __init__(self):
@@ -11,3 +16,7 @@ class AppLibrary:
 
     def load_test_data(self):
         os.system(f"{os.getenv('PSQL_SCHEMA_COMMAND','psql -f')} src/tests/test-schema.sql")
+
+    def read_generated_file(self):
+        downloads_path = str(Path.home() / 'Downloads')
+        generated_output = open(str(downloads_path / 'exportedReferences.bib')).read()
