f3466da4c6162ff7edd7ee9485d4f7b929ede0af
Ismo Mehdi <84803085+ismomehdi@users.noreply.github.com>
2023-12-10 15:59:54 +0200

    Move frontend component handling functions to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,11 @@ import AppTitle from './components/AppTitle'
 import DoiForm from './components/DoiForm'
 import FormContainer from './components/ReferenceForm/FormContainer'
 import ReferenceTable from './components/ReferenceTable/ReferenceTable'
+import { addReference } from './api/referenceService'
+import { deleteReferencesInArray } from './api/referenceService'
 import { fetchAllReferences } from './util/fetchUtil'
 import { parseDoiMetaData } from './util/DoiUtil'
+import { convertToBibTex } from './util/bibTexUtil'
 
 const App = () => {
     const [rows, setRows] = useState([])
@@ -32,9 +35,45 @@ const App = () => {
         setReferenceFormData({})
     }
 
-    const handleDelete = async () => {
-        const references = await fetchAllReferences()
-        references && setRows(references)
+    const handleDelete = async (selected) => {
+        try {
+            await deleteReferencesInArray(selected)
+            const references = await fetchAllReferences()
+            references && setRows(references)
+        } catch (error) {
+            console.error('Error fetching data: ', error)
+        }
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        const form = new FormData(event.target)
+
+        try {
+            await addReference(form)
+            handleReferenceAdded()
+            alert('Reference added!')
+        } catch (error) {
+            console.error('Submit failed:', error)
+            alert('Submit failed.')
+        }
+    }
+
+    const handleDownload = async (selected) => {
+        const selectedRows = rows.filter((row) =>
+            selected.includes(row.book_id)
+        )
+
+        const bibTexString = convertToBibTex(selectedRows)
+        const fileToDownload = new Blob([bibTexString], { type: 'text/plain' })
+
+        const downloadLink = document.createElement('a')
+        downloadLink.href = URL.createObjectURL(fileToDownload)
+        downloadLink.download = 'exportedReferences.bib'
+
+        document.body.appendChild(downloadLink)
+        downloadLink.click()
+        document.body.removeChild(downloadLink)
     }
 
     return (
@@ -46,10 +85,15 @@ const App = () => {
                 onReferenceAdded={handleReferenceAdded}
                 formData={referenceFormData}
                 onInputChange={handleReferenceFormInputChange}
+                handleSubmit={handleSubmit}
             />
             <br />
             <br />
-            <ReferenceTable rows={rows} onDelete={handleDelete} />
+            <ReferenceTable
+                rows={rows}
+                onDelete={handleDelete}
+                onDownload={handleDownload}
+            />
         </div>
     )
 }
--- frontend/src/components/ReferenceForm/FormContainer.js
+++ frontend/src/components/ReferenceForm/FormContainer.js
@@ -2,25 +2,10 @@ import React, { useState } from 'react'
 import Dropdown from './Dropdown'
 import BookForm from './BookForm'
 import ArticleForm from './ArticleForm'
-import { addReference } from '../../api/referenceService'
 
 const FormContainer = (props) => {
-    const { onReferenceAdded, formData, onInputChange } = props
+    const { formData, onInputChange, handleSubmit } = props
     const [referenceType, setReferenceType] = useState('Book')
-
-    const handleSubmit = async (event) => {
-        event.preventDefault()
-        const form = new FormData(event.target)
-
-        try {
-            await addReference(form)
-            onReferenceAdded()
-            alert('Reference added!')
-        } catch (error) {
-            console.error('Submit failed:', error)
-            alert('Submit failed.')
-        }
-    }
 
     const handleDropdownChange = (selectedValue) => {
         setReferenceType(selectedValue)
