1947bdaa8aebe8fe187938dd7d25321575070bce
Tartsi <tarvo.lilja@hotmail.fi>
2023-12-11 23:48:04 +0200

    Rename tests

--- src/tests/services/test_reference_services.py
+++ src/tests/services/test_reference_services.py
@@ -6,55 +6,51 @@ from services.reference_services import ReferenceServices
 class TestReferenceService(unittest.TestCase):
     def setUp(self):
         self.mock_dataprocessing = mock.Mock()
-        self.mock_dataprocessing.get_all_books()
+        self.mock_dataprocessing.get_all_references()
 
-        #setting return value to False indicating that a book with the same title do not exist in beginning
-        self.mock_dataprocessing.get_book_by_title.return_value = False
+        # setting return value to False indicating that a reference with the same title do not exist in beginning
+        self.mock_dataprocessing.get_reference_by_title.return_value = False
 
         self.ref_ser = ReferenceServices(self.mock_dataprocessing)
 
-    def test_add_valid_book(self):
-        self.assertTrue(self.ref_ser.add_book(authors='Garwin, Robert',
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
-                                              ref_type="book"))
+    def test_add_valid_reference(self):
+        self.assertTrue(self.ref_ser.add_reference(authors='Garwin, Robert',
+                                                   title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                   year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
+                                                   ref_type="book"))
 
     def test_add_book_with_invalid_year(self):
-        self.assertFalse(self.ref_ser.add_book(authors='Garwin, Robert',
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year=1000, publisher='Prentice Hall', publisher_address='Bakerstreet 123'))
+        self.assertFalse(self.ref_ser.add_reference(authors='Garwin, Robert',
+                                                    title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                    year=1000, publisher='Prentice Hall', publisher_address='Bakerstreet 123', ref_type="book"))
 
     def test_add_book_with_str_type_year(self):
-        self.assertFalse(self.ref_ser.add_book(authors='Garwin, Robert',
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year='1000', publisher='Prentice Hall', publisher_address='Bakerstreet 123'))
+        self.assertFalse(self.ref_ser.add_reference(authors='Garwin, Robert',
+                                                    title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                    year='1000', publisher='Prentice Hall', publisher_address='Bakerstreet 123', ref_type="book"))
 
     def test_add_book_with_invalid_author(self):
-        self.assertFalse(self.ref_ser.add_book(authors=None,
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123'))
-
-    def test_get_all_references_calls_get_all_books(self):
-        self.ref_ser.get_all_references()
-        self.mock_dataprocessing.get_all_books.assert_called()
+        self.assertFalse(self.ref_ser.add_reference(authors=None,
+                                                    title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                    year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123', ref_type="book"))
 
     def test_add_booktitle_twice(self):
-        self.assertTrue(self.ref_ser.add_book(authors='Garwin, Robert',
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
-                                              ref_type='book'))
+        self.assertTrue(self.ref_ser.add_reference(authors='Garwin, Robert',
+                                                   title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                   year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
+                                                   ref_type='book'))
 
-        #setting return value to True indicating that a book with the same title do exist after added title
-        self.mock_dataprocessing.get_book_by_title.return_value = True
-        self.assertFalse(self.ref_ser.add_book(authors='Garwin, Robert',
-                                              title='Clean Code: A Handbook of Agile Software Craftsmanship',
-                                              year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
-                                              ref_type='book'))
+        # setting return value to True indicating that a book with the same title do exist after added title
+        self.mock_dataprocessing.get_reference_by_title.return_value = True
+        self.assertFalse(self.ref_ser.add_reference(authors='Garwin, Robert',
+                                                    title='Clean Code: A Handbook of Agile Software Craftsmanship',
+                                                    year=2008, publisher='Prentice Hall', publisher_address='Bakerstreet 123',
+                                                    ref_type='book'))
 
-    def test_delete_all_books(self):
-        self.ref_ser.delete_all_books()
-        self.mock_dataprocessing.delete_all_books.assert_called()
+    def test_delete_all_references(self):
+        self.ref_ser.delete_all_references()
+        self.mock_dataprocessing.delete_all_references.assert_called()
 
-    def test_delete_books_by_id(self):
-        self.ref_ser.delete_books_by_id([1, 2, 3])
-        self.mock_dataprocessing.delete_books_by_id.assert_called()
+    def test_delete_references_by_id(self):
+        self.ref_ser.delete_references_by_id([1, 2, 3])
+        self.mock_dataprocessing.delete_references_by_id.assert_called()
--- src/tests/test_dataprocessing.py
+++ src/tests/test_dataprocessing.py
@@ -14,91 +14,93 @@ class TestDataProcessing(unittest.TestCase):
         os.system(
             f"{os.getenv('PSQL_SCHEMA_COMMAND','psql -d testdb -f')} src/tests/test-schema.sql")
 
-    def test_add_book(self):
+    def test_add_reference(self):
         with app.app_context():
             self.assertEqual(True, dataprocessing.add_reference(
                 author="Wincewind", title="My Life", year=2000, publisher="My mom",
                 publisher_address="123 Noway Street"))
 
-    def test_fail_to_add_book(self):
+    def test_fail_to_add_reference(self):
         with app.app_context():
             self.assertEqual(False, dataprocessing.add_reference())
 
-    def test_get_all_books(self):
+    def test_get_all_references(self):
         with app.app_context():
-            self.assertEqual(len(dataprocessing.get_all_books()), 2)
+            self.assertEqual(len(dataprocessing.get_all_references()), 2)
 
     @patch('dataprocessing.db.session.execute')
-    def test_fail_to_get_all_books(self, mock_execute):
+    def test_fail_to_get_all_references(self, mock_execute):
         with app.app_context():
             mock_execute.side_effect = Exception("Database error")
-            result = dataprocessing.get_all_books()
+            result = dataprocessing.get_all_references()
             self.assertEqual(result, [])
             mock_execute.assert_called_once()
 
-    def test_delete_all_books(self):
+    def test_delete_all_references(self):
         with app.app_context():
             dataprocessing.add_reference(
-                author="Wincewind", title="My Life", year=2000, publisher="My Dad", 
+                author="Wincewind", title="My Life", year=2000, publisher="My Dad",
                 publisher_address="123 Noway Street")
-            self.assertEqual([], dataprocessing.delete_all_books())
+            self.assertEqual([], dataprocessing.delete_all_references())
 
     @patch('dataprocessing.db.session.execute')
-    def test_fail_to_delete_books(self, mock_execute):
+    def test_fail_to_delete_references(self, mock_execute):
         with app.app_context():
             mock_execute.side_effect = Exception("Database error")
-            result = dataprocessing.delete_all_books()
+            result = dataprocessing.delete_all_references()
             self.assertEqual(result, [])
             self.assertEqual(mock_execute.call_count, 2)
 
-    def test_delete_single_book(self):
+    def test_delete_single_reference(self):
         with app.app_context():
             dataprocessing.add_reference(
-                author="Wincewind", title="My Life", year=2000, publisher="My Dad", 
+                author="Wincewind", title="My Life", year=2000, publisher="My Dad",
                 publisher_address="123 Noway Street")
-            # Get book from database (listed dictionary)
-            result = dataprocessing.get_all_books()
-            # Return the id of the book as a list
-            self.assertEqual(True, dataprocessing.delete_books_by_id([result[0]['id']]))
-            self.assertEqual(2, len(dataprocessing.get_all_books()))
+            # Get reference from database (listed dictionary)
+            result = dataprocessing.get_all_references()
+            # Return the id of the reference as a list
+            self.assertEqual(
+                True, dataprocessing.delete_references_by_id([result[0]['id']]))
+            self.assertEqual(2, len(dataprocessing.get_all_references()))
 
-    def test_delete_multiple_books(self):
+    def test_delete_multiple_references(self):
         with app.app_context():
             dataprocessing.add_reference(
                 author="Hawking, Stephen", title="Brief Answers to the Big Questions", year=2018,
                 publisher="Murray, John", publisher_address="338 Euston Road London")
             dataprocessing.add_reference(
-                author="Wincewind", title="My Life", year=2000, publisher="My Dad", 
+                author="Wincewind", title="My Life", year=2000, publisher="My Dad",
                 publisher_address="123 Noway Street")
-            # Returns both books from database (listed dictionary)
-            result = dataprocessing.get_all_books()
-            # Get the ids of the books as a list
+            # Returns references from database (listed dictionary)
+            result = dataprocessing.get_all_references()
+            # Get the ids of the references as a list
             ids = [id['id'] for id in result]
-            self.assertEqual(True, dataprocessing.delete_books_by_id(ids))
-            self.assertEqual(0, len(dataprocessing.get_all_books()))
+            self.assertEqual(True, dataprocessing.delete_references_by_id(ids))
+            self.assertEqual(0, len(dataprocessing.get_all_references()))
 
     @patch('dataprocessing.db.session.execute')
-    def test_fail_to_delete_books_by_id(self, mock_execute):
+    def test_fail_to_delete_references_by_id(self, mock_execute):
         with app.app_context():
             mock_execute.side_effect = Exception("Database error")
-            self.assertFalse(dataprocessing.delete_books_by_id([1]))
+            self.assertFalse(dataprocessing.delete_references_by_id([1]))
             mock_execute.assert_called_once()
 
-    def test_get_book_by_title(self):
+    def test_get_reference_by_title(self):
         with app.app_context():
             dataprocessing.add_reference(
                 author="Wincewind", title="My Life", year=2000, publisher="My Dad",
                 publisher_address="123 Noway Street")
-            self.assertEqual(True, dataprocessing.get_book_by_title("My Life"))
+            self.assertEqual(
+                True, dataprocessing.get_reference_by_title("My Life"))
 
-    def test_fail_to_get_book_by_title(self):
+    def test_fail_to_get_reference_by_title(self):
         with app.app_context():
             self.assertEqual(
-                False, dataprocessing.get_book_by_title("Your Life"))
+                False, dataprocessing.get_reference_by_title("Your Life"))
 
     @patch('dataprocessing.db.session.execute')
-    def test_fail_to_get_book_by_title_database_error(self, mock_execute):
+    def test_fail_to_get_reference_by_title_database_error(self, mock_execute):
         with app.app_context():
             mock_execute.side_effect = Exception("Database error")
-            result = dataprocessing.get_book_by_title("Your Life")
+            result = dataprocessing.get_reference_by_title("Your Life")
             self.assertEqual(result, False)
